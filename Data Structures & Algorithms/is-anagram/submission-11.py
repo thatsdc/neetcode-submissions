fe541301d754d376e855013a042434c3f729b7b0
Daniele Caldarigi <danielecaldarigi05@gmail.com>
2026-07-30 13:57:25 +0200

    Add: is-anagram - submission-11

---
+++ Data Structures & Algorithms/is-anagram/submission-11.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # O(n + m)
+        if len(s) != len(t): return False
+
+     
+        return sorted(list(s)) == sorted(list(t))
