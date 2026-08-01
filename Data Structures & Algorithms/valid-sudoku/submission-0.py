aca1c682eb34bd886cbf53f44429be9a6f3f99e1
Daniele Caldarigi <danielecaldarigi05@gmail.com>
2026-08-01 12:15:53 +0200

    Add: valid-sudoku - submission-0

---
+++ Data Structures & Algorithms/valid-sudoku/submission-0.py
@@ -0,0 +1,38 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        column = [[] for i in range(len(board))]
+        boxes = [[] for i in range(len(board))]
+
+        for i in range(len(board)):
+            temp = []
+            for j in range(len(board)):
+                cell_value = board[i][j]
+                if cell_value == ".": continue
+
+                # Line check
+                if cell_value in temp:
+                    return False
+                else: 
+                    temp.append(cell_value)
+
+                # Column check
+                if cell_value in column[j]:
+                    return False
+                else: 
+                    column[j].append(cell_value)
+
+                # Box check
+                a = int(j/3)
+                b = int(i/3)
+                box_idx = int(3 * b + a)
+                print(i, j)
+                print(box_idx)
+                if cell_value in boxes[box_idx]: 
+                    return False
+                else: 
+                    boxes[box_idx].append(cell_value)
+
+        return True
+                
+
+            
