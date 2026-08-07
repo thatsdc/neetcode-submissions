1f7e0a4ce9867a8e9cb654324a69b1275f7a8257
Daniele Caldarigi <danielecaldarigi05@gmail.com>
2026-08-07 17:46:13 +0200

    Add: buy-and-sell-crypto - submission-4

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-4.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxProfit(self, prices: list[int]) -> int:
+        min_v = 0
+        max_v = 0
+        temp_min = 1000
+        temp_max = 0
+
+        for i in range(len(prices)):
+            p = prices[i]
+            if p < temp_min:
+                temp_min = p
+                temp_max = 0
+
+            if p > temp_max:
+                temp_max = p
+            
+            if temp_max - temp_min > max_v - min_v: 
+                min_v = temp_min
+                max_v = temp_max
+
+        return max_v - min_v
